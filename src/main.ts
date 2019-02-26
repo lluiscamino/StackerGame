795e80e7387fb59bd32714144068630eeea5641e
lluis <caminoperez.lluis@gmail.com>
2019-02-26 21:45:57 +0100

    Configuration page (modify speed & background color)

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ Game.generateBlocks();
 document.getElementById('record').innerText = String(Game.record);
 document.getElementById('gameClickable').addEventListener('click', Game.newGame);
 document.addEventListener('keydown', Game.newGame);
+document.getElementById('configGear').addEventListener('click', Game.handleConfigClick);
+document.getElementById('configForm').addEventListener('submit', Game.handleConfigUpdate);
